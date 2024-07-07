c9d6fe4c904ce826f340ed58d4dcc9bb02eb0f72
a1802961 <a1802961@student.adelaide.edu.au>
2024-07-07 19:02:10 +0930

    Created the models for blogs

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -11,9 +11,31 @@ class Category(models.Model):
     
 
 
-class BlogPost(models.Model):
+class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_queryset(self) -> models.QuerySet:
+            return super().get_queryset() .filter(status='published')
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     excerpt = models.TextField(null=True)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date='published')
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    status = models.CharField(max_length=10, choices=options, default='published')
+
+    objects = models.Manager() # Default Manager
+    postobjects = PostObjects() #Custom Manager
+
+
+    class Meta:
+        ordering = ('published',)
+
     def __str__(self):
-        return self.name
+        return self.title
