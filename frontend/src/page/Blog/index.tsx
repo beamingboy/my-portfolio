6fb7b75cdf2a6d4c2e1b3218f28e35251dd14405
a1802961 <a1802961@student.adelaide.edu.au>
2024-09-07 20:41:57 +0930

    Setup react app with typescript

---
+++ frontend/src/page/Blog/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function Blog(){
+  return (
+    <div>
+      <h1>My Blog</h1>
+      <p>Here are some of my blog posts.</p>
+    </div>
+  );
+};
+
+export default Blog;
---
+++ frontend/src/route/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Router } from 'react-router-dom';
+import { Home, Blog } from '../page';
+
+const AppRoutes: React.FC = () => {
+  return (
+    // <Router>
+        <></>
+    //     <Route exact path="/" component={Home} />
+    //   <Route path="/blog" component={Blog} />
+    // </Router>
+  );
+};
+
+export default AppRoutes;
